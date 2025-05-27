f6fca642b97918cb14b8aafe6d795433d7049c28
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2025-05-27 19:36:30 +0700

    11: products

--- src/app/(frontend)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(frontend)/(home)/[category]/[subcategory]/page.tsx
@@ -1,17 +1,35 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import {
+	ProductList,
+	ProductListSkeleton,
+} from '@/modules/products/ui/components/product-list';
+
 type Props = {
 	params: Promise<{
-		category: string;
 		subcategory: string;
 	}>;
 };
 
-export default async function CategoryPage({ params }: Props) {
-	const { category, subcategory } = await params;
+export default async function SubcategoryPage({ params }: Props) {
+	const { subcategory } = await params;
+
+	const queryClient = getQueryClient();
+
+	void queryClient.prefetchQuery(
+		trpc.products.getMany.queryOptions({ category: subcategory })
+	);
 	return (
-		<div>
-			Category: {category}
-			<br />
-			Subcategory: {subcategory}
-		</div>
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<ErrorBoundary fallback={<div>Something went wrong</div>}>
+				<Suspense fallback={<ProductListSkeleton />}>
+					<ProductList category={subcategory} />
+				</Suspense>
+			</ErrorBoundary>
+		</HydrationBoundary>
 	);
 }
