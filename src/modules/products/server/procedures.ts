e76fad5f0b57cdfb7df1c97e78d3970f28bbeb68
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2025-06-06 23:30:47 +0700

    21: library

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,142 +1,172 @@
-import type { Sort, Where } from 'payload';
-import { z } from 'zod';
+import { headers as getHeaders } from "next/headers";
+import type { Sort, Where } from "payload";
+import { z } from "zod";
 
-import { Category, Media, Tenant } from '@/payload-types';
-import { baseProcedure, createTRPCRouter } from '@/trpc/init';
+import { Category, Media, Tenant } from "@/payload-types";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 
-import { DEFAULT_LIMIT } from '@/constants';
+import { DEFAULT_LIMIT } from "@/constants";
 
-import { sortValues } from '@/modules/products/search-params';
+import { sortValues } from "@/modules/products/search-params";
 
 export const productsRouter = createTRPCRouter({
-	getOne: baseProcedure
-		.input(
-			z.object({
-				id: z.string(),
-			})
-		)
-		.query(async ({ ctx, input }) => {
-			const product = await ctx.payload.findByID({
-				collection: 'products',
-				id: input.id,
-				depth: 2, // load the "product.image", "product.tenant", and "product.tenant.image"
-			});
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const headers = await getHeaders();
+      const session = await ctx.payload.auth({ headers });
 
-			return {
-				...product,
-				image: product.image as Media | null,
-				tenant: product.tenant as Tenant & { image: Media | null },
-			};
-		}),
+      const product = await ctx.payload.findByID({
+        collection: "products",
+        id: input.id,
+        depth: 2, // load the "product.image", "product.tenant", and "product.tenant.image"
+      });
 
-	getMany: baseProcedure
-		.input(
-			z.object({
-				cursor: z.number().default(1),
-				limit: z.number().default(DEFAULT_LIMIT),
-				category: z.string().nullable().optional(),
-				minPrice: z.string().nullable().optional(),
-				maxPrice: z.string().nullable().optional(),
-				tags: z.array(z.string()).nullable().optional(),
-				sort: z.enum(sortValues).nullable().optional(),
-				tenantSlug: z.string().nullable().optional(),
-			})
-		)
-		.query(async ({ ctx, input }) => {
-			const where: Where = {};
-			let sort: Sort = '-createdAt';
+      let isPurchased = false;
+      if (session.user) {
+        const ordersData = await ctx.payload.find({
+          collection: "orders",
+          pagination: false,
+          limit: 1,
+          where: {
+            and: [
+              {
+                product: {
+                  equals: input.id,
+                },
+              },
+              {
+                user: {
+                  equals: session.user.id,
+                },
+              },
+            ],
+          },
+        });
 
-			if (input.sort === 'curated') {
-				sort = '-createdAt';
-			}
-			if (input.sort === 'trending') {
-				sort = '-createdAt';
-			}
-			if (input.sort === 'hot_and_new') {
-				sort = '+createdAt';
-			}
+        isPurchased = !!ordersData.docs[0];
+      }
 
-			if (input.minPrice && input.maxPrice) {
-				where.price = {
-					greater_than_equal: input.minPrice,
-					less_than_equal: input.maxPrice,
-				};
-			} else if (input.minPrice) {
-				where.price = {
-					greater_than_equal: input.minPrice,
-				};
-			} else if (input.maxPrice) {
-				where.price = {
-					less_than_equal: input.maxPrice,
-				};
-			}
+      return {
+        ...product,
+        isPurchased,
+        image: product.image as Media | null,
+        tenant: product.tenant as Tenant & { image: Media | null },
+      };
+    }),
 
-			if (input.tenantSlug) {
-				where['tenant.slug'] = {
-					equals: input.tenantSlug,
-				};
-			}
+  getMany: baseProcedure
+    .input(
+      z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
+        category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
+        tags: z.array(z.string()).nullable().optional(),
+        sort: z.enum(sortValues).nullable().optional(),
+        tenantSlug: z.string().nullable().optional(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const where: Where = {};
+      let sort: Sort = "-createdAt";
 
-			if (input.category) {
-				const categoriesData = await ctx.payload.find({
-					collection: 'categories',
-					limit: 1,
-					depth: 1, // populate subcategories, subcategories.[0] will be a type of "Category"
-					pagination: false,
-					where: {
-						slug: {
-							equals: input.category,
-						},
-					},
-				});
+      if (input.sort === "curated") {
+        sort = "-createdAt";
+      }
+      if (input.sort === "trending") {
+        sort = "-createdAt";
+      }
+      if (input.sort === "hot_and_new") {
+        sort = "+createdAt";
+      }
 
-				const formatedData = categoriesData.docs.map((doc) => ({
-					...doc,
-					subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-						// because of "depth: 1" we are confident doc will be a type of "Category"
-						...(doc as Category),
-						subcategories: undefined,
-					})),
-				}));
+      if (input.minPrice && input.maxPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+          less_than_equal: input.maxPrice,
+        };
+      } else if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+        };
+      }
 
-				const subcategoriesSlugs = [];
-				const parentCategory = formatedData[0];
+      if (input.tenantSlug) {
+        where["tenant.slug"] = {
+          equals: input.tenantSlug,
+        };
+      }
 
-				if (parentCategory) {
-					subcategoriesSlugs.push(
-						...parentCategory.subcategories.map(
-							(subcategory) => subcategory.slug
-						)
-					);
+      if (input.category) {
+        const categoriesData = await ctx.payload.find({
+          collection: "categories",
+          limit: 1,
+          depth: 1, // populate subcategories, subcategories.[0] will be a type of "Category"
+          pagination: false,
+          where: {
+            slug: {
+              equals: input.category,
+            },
+          },
+        });
 
-					where['category.slug'] = {
-						in: [parentCategory.slug, ...subcategoriesSlugs],
-					};
-				}
-			}
+        const formatedData = categoriesData.docs.map((doc) => ({
+          ...doc,
+          subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            // because of "depth: 1" we are confident doc will be a type of "Category"
+            ...(doc as Category),
+            subcategories: undefined,
+          })),
+        }));
 
-			if (input.tags && input.tags.length > 0) {
-				where['tags.name'] = {
-					in: input.tags,
-				};
-			}
+        const subcategoriesSlugs = [];
+        const parentCategory = formatedData[0];
 
-			const data = await ctx.payload.find({
-				collection: 'products',
-				depth: 2, // populate "category", "image", "tenant" & "tenant.image"
-				where,
-				sort,
-				page: input.cursor,
-				limit: input.limit,
-			});
+        if (parentCategory) {
+          subcategoriesSlugs.push(
+            ...parentCategory.subcategories.map(
+              (subcategory) => subcategory.slug,
+            ),
+          );
 
-			return {
-				...data,
-				docs: data.docs.map((doc) => ({
-					...doc,
-					image: doc.image as Media | null,
-					tenant: doc.tenant as Tenant & { image: Media | null },
-				})),
-			};
-		}),
+          where["category.slug"] = {
+            in: [parentCategory.slug, ...subcategoriesSlugs],
+          };
+        }
+      }
+
+      if (input.tags && input.tags.length > 0) {
+        where["tags.name"] = {
+          in: input.tags,
+        };
+      }
+
+      const data = await ctx.payload.find({
+        collection: "products",
+        depth: 2, // populate "category", "image", "tenant" & "tenant.image"
+        where,
+        sort,
+        page: input.cursor,
+        limit: input.limit,
+      });
+
+      return {
+        ...data,
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null,
+          tenant: doc.tenant as Tenant & { image: Media | null },
+        })),
+      };
+    }),
 });
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,20 @@
-import { authRouter } from '@/modules/auth/server/procedures';
-import { categoriesRouter } from '@/modules/categories/server/procedures';
-import { checkoutRouter } from '@/modules/checkout/server/procedures';
-import { productsRouter } from '@/modules/products/server/procedures';
-import { tagsRouter } from '@/modules/tags/server/procedures';
-import { tenantsRouter } from '@/modules/tenants/server/procedures';
+import { authRouter } from "@/modules/auth/server/procedures";
+import { categoriesRouter } from "@/modules/categories/server/procedures";
+import { checkoutRouter } from "@/modules/checkout/server/procedures";
+import { LibraryRouter } from "@/modules/library/server/procedures";
+import { productsRouter } from "@/modules/products/server/procedures";
+import { tagsRouter } from "@/modules/tags/server/procedures";
+import { tenantsRouter } from "@/modules/tenants/server/procedures";
 
-import { createTRPCRouter } from '../init';
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-	auth: authRouter,
-	categories: categoriesRouter,
-	products: productsRouter,
-	tags: tagsRouter,
-	tenants: tenantsRouter,
-	checkout: checkoutRouter,
+  auth: authRouter,
+  categories: categoriesRouter,
+  products: productsRouter,
+  tags: tagsRouter,
+  tenants: tenantsRouter,
+  checkout: checkoutRouter,
+  library: LibraryRouter,
 });
 export type AppRouter = typeof appRouter;
